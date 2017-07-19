4f2f79faf8bd2fab20bf31bd380865f595708c7d
Ann Shopina <annshopina@users.noreply.github.com>
2017-07-19 20:22:47 +0300

    Add files via upload
    123

---
+++ NewLesson6/NewLesson6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace NewLesson6
+{
+
+    class MainClass
+    {
+        public struct City
+        {
+            public string Name;
+            public int Population;
+            public int Density;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            string Userdata = Console.ReadLine();
+            string[] CityInfo = Userdata.Split(';');
+
+            City[] cities = new City[CityInfo.Length];
+            int citynum = 0;
+            for (var i = 0; i < CityInfo.Length; i++)
+            {
+                string[] cityParts = CityInfo[i].Split('=');
+                cities[citynum] = new City();
+                cities[citynum].Name = cityParts[0];
+                string[] cityData = cityParts[1].Split(',');
+
+                cities[citynum].Population = Int32.Parse(cityData[0]);
+                cities[citynum].Density = Int32.Parse(cityData[1]);
+                Console.WriteLine(cities[citynum].Density);
+                Console.WriteLine(cities[citynum].Population);
+                Console.WriteLine(cities[citynum].Name);
+                citynum++;
+            }
+            int maxPopulation = cities[0].Population;
+            int maxPopulationIndex = 0;
+
+            for (int i = 0; i < cities.Length; i++)
+            {
+                if (cities[i].Population > maxPopulation)
+                {
+                    maxPopulation = cities[i].Population;
+                    maxPopulationIndex = i;
+                }
+                Console.WriteLine(maxPopulation);
+
+            }
+        }
+    }
+}
+
+
+
+
+        
+       
+    
